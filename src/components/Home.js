685df1d806db6fee121bfada522f38ce248ac4c5
Kaisar Ahmmed <kaisar123k@gmail.com>
2022-02-27 14:48:21 +0600

    add movie query hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -6,14 +6,15 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Componets
 
 // Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 // Image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const { state, loading, error } = useHomeFetch();
+
+    console.log(state);
 
     return <div>Home Page</div>;
 };
